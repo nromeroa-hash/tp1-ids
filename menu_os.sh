2cc4142c18b61d923208c22b106d075c55b6242e
nromeroa <nromeroa@fi.uba.ar>
2026-03-24 12:25:32 -0300

    Update menu_os.sh

--- menu_os.sh
+++ menu_os.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-parametro=$1
+if [ "$1" = "-d" ]; then
+    rm -rf ~/EPNro1
+    pkill -f consolidar.sh
+    exit 0
+fi
 
-while true; 
+while [ "$opcion" -ne 6 ]; 
 do
     echo "-----MENU-----"
     echo "1) Crear entorno"
@@ -19,25 +23,38 @@ do
         1)
            mkdir -p ~/EPNro1/{entrada, salida, procesado}
            echo "Se creó el entorno en ~/EPNro1"
-	   ;;
+           ;;
 
         2)
-	   if [ ! -d ~/EPNro1 ]; then
-		echo "Para realizar esta opción primero debe seleccionar la opción 1"
-	   else
-		echo "Corriendo proceso"
-		nohup ~/EPNro1/consolidar.sh &
-	   fi ;;
+           if [ ! -d ~/EPNro1 ]; then
+                echo "Para realizar esta opción primero debe seleccionar la opción 1"
+           else
+                echo "Corriendo proceso"
+                nohup ~/EPNro1/consolidar.sh &
+           fi 
+           ;;
 
-        3) --
+        3)
+           if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
+                sort -n ~/EPNro1/salida/"$FILENAME".txt
+           fi
+           ;;
 
         4)
+           if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
+                sort -k4  -n  -r ~/EPNro1/salida/"$FILENAME".txt | head -10
+           fi
+           ;;
 
         5)
+           echo -n "Ingrese un número de padrón: "
+           read padron
+           grep "$padron" ~/EPNro1/salida/"$FILENAME".txt
+           ;;
 
         6)
-	   echo "Saliendo..."
-           exit 0;;
-
+           echo "Saliendo..."
+           ;;
     esac
 done
+
