c5b2b00b1cfda6b0238d8f5843f1f3c79184a50c
Fernanda Torres <fernandainestorres@outlook.com>
2026-03-25 00:36:08 -0300

    Agregé mensaje al usuario en opc 3, 4 y 5; condicional en opc 5; corregí identaciones

--- menu_os.sh
+++ menu_os.sh
@@ -5,64 +5,71 @@ opcion=0
 
 while [ "$opcion" -ne 6 ]; 
 do
-    echo "-----MENU-----"
-    echo "1) Crear entorno"
-    echo "2) Correr proceso"
-    echo "3) Ver listado de alumnos ordenados por padrón"
-    echo "4) Ver 10 notas más altas del listado de alumnos"
-    echo "5) Ver datos del alumno con padrón: "
-    echo "6) Salir"
+   echo "-----MENU-----"
+   echo "1) Crear entorno"
+   echo "2) Correr proceso"
+   echo "3) Ver listado de alumnos ordenados por padrón"
+   echo "4) Ver 10 notas más altas del listado de alumnos"
+   echo "5) Ver datos del alumno con padrón: "
+   echo "6) Salir"
 
-    echo -n "Ingrese la opción del menú: "
-    read opcion
+   echo -n "Ingrese la opción del menú: "
+   read opcion
 
-    case $opcion in 
-        1)
-           mkdir -p ~/EPNro1/{entrada,salida,procesado}
-           echo "Se creó el entorno en ~/EPNro1"
-           ;;
+      case $opcion in 
+         1)
+            mkdir -p ~/EPNro1/{entrada,salida,procesado}
+            echo "Se creó el entorno en ~/EPNro1"
+            ;;
 
-        2)
-           if [ ! -d ~/EPNro1 ]; then
+         2)
+            if [ ! -d ~/EPNro1 ]; then
                echo "Para realizar esta opción primero debe seleccionar la opción 1"
-           else
+            else
   
-                if [ ! -f ~/EPNro1/consolidar.sh ]; then
-                    cp consolidar.sh ~/EPNro1/
-                    chmod +x ~/EPNro1/consolidar.sh
-                fi
+               if [ ! -f ~/EPNro1/consolidar.sh ]; then
+                  cp consolidar.sh ~/EPNro1/
+                  chmod +x ~/EPNro1/consolidar.sh
+               fi
 
-                echo "Corriendo proceso"
-                nohup ~/EPNro1/consolidar.sh > /dev/null 2>&1 &
-           fi
-           ;;
+               echo "Corriendo proceso"
+               nohup ~/EPNro1/consolidar.sh > /dev/null 2>&1 &
+            fi
+            ;;
 
-        3)
-           if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
-                sort -n ~/EPNro1/salida/"$FILENAME".txt
-           fi
-           ;;
+         3)
+            if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
+               sort -n ~/EPNro1/salida/"$FILENAME".txt
+            else 
+               echo "No existe el archivo. Primero se debe ejecutar opciones 1 y 2"
+            fi
+            ;;
 
-        4)
-           if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
+         4)
+            if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
                 sort -k5  -n  -r ~/EPNro1/salida/"$FILENAME".txt | head -10
-           fi
-           ;;
+            else 
+               echo "No existe el archivo. Primero se debe ejecutar opciones 1 y 2"
+            fi
+            ;;
 
-        5)
-           echo -n "Ingrese un número de padrón: "
-           read padron
-           grep "$padron" ~/EPNro1/salida/"$FILENAME".txt
-           ;;
+         5)
+            if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
+               echo -n "Ingrese un número de padrón: "
+               read padron
+               grep "$padron" ~/EPNro1/salida/"$FILENAME".txt
+            else echo "No existe el archivo. Primero se debe ejecutar opciones 1 y 2" 
+            fi
+            ;;
 
-        6)
-           echo "Saliendo..."
-           ;;
-    esac
+         6)
+            echo "Saliendo..."
+            ;;
+      esac
 done
 
 if [ "$parametro" = "-d" ]; then
-    echo "Eliminando entorno..."
-    rm -rf ~/EPNro1
-    pkill -f consolidar.sh
+   echo "Eliminando entorno..."
+   rm -rf ~/EPNro1
+   pkill -f consolidar.sh
 fi
